660254eb5fed47300086d8e13337541936a3c1ab
alanmartinsedna <alan.martins@edna.center>
2026-04-27 12:58:25 -0300

    removido console log

--- static/assets/js/components/get-name-economic-group.js
+++ static/assets/js/components/get-name-economic-group.js
@@ -1,7 +1,5 @@
 export function getNameEconomicGroup(data) {
-    console.log('IMPORTADO SCRIPT DE ARQUIVO EXTERNO - Dados recebidos para extração do nome:', data);
     let economicGroupName = data.reportData[0].economicGroup;
     economicGroupName = economicGroupName != '' ? economicGroupName : 'Grupo Economico não localizado';
-
     return economicGroupName;
 }
--- static/assets/js/components/get-name-eval-diag.js
+++ static/assets/js/components/get-name-eval-diag.js
@@ -1,7 +1,5 @@
 export function getNameEvaluationDiagnostic(data) {
-    console.log('IMPORTADO SCRIPT DE ARQUIVO EXTERNO - Dados recebidos para extração do nome:', data);
     let fullName = data.reportData[0].methodName;
-    fullName = fullName != '' ? fullName : 'Diagnóstico de Riscos Psicossociais VALOR DA VARIAVEL VAZIA';
-
+    fullName = fullName != '' ? fullName : 'Diagnóstico de Riscos Psicossociais';
     return fullName;
 }
