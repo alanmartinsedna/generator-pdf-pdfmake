07099383eb25fb6557eb0a93cdc3a1743823397f
alanmartinsedna <alan.martins@edna.center>
2026-04-27 12:11:17 -0300

    Adicionado rotas alternativas

--- pdfgenerator/urls.py
+++ pdfgenerator/urls.py
@@ -3,4 +3,10 @@ from .views import tela_relatorio
 
 urlpatterns = [
     path('relatorio/', tela_relatorio, name='pdf_generator'),
+    path('diagnostico/aderencia-participacao/', tela_relatorio, name='diagnostico_aderencia_participacao'),
+    path('diagnostico/respostas/', tela_relatorio, name='diagnostico_respostas'),
+    path('diagnostico/recomendacoes/', tela_relatorio, name='diagnostico_recomendacoes'),
+    path('avaliacao/aderencia-participacao/', tela_relatorio, name='avaliacao_aderencia_participacao'),
+    path('avaliacao/respostas/', tela_relatorio, name='avaliacao_respostas'),
+    path('avaliacao/recomendacoes/', tela_relatorio, name='avaliacao_recomendacoes'),
 ]
